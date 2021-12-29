22c47d54abb72df6a9560c5f4703593782eb817e
Junx <hugh8845sam@gmail.com>
2021-12-29 21:29:43 +0800

    rename

--- cli.py
+++ cli.py
@@ -1884,7 +1884,7 @@ def google_new(key, url, num, save):
                 '</tbody>'
                 '</table>')
 
-        f = open(os.path.join(save, f'{key}-news.html'), "w", encoding="utf-8")
+        f = open(os.path.join(save, f'{key}-{time.time()}.html'), "w", encoding="utf-8")
         f.write(html)
         f.close()
     else:
